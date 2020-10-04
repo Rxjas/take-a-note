ab7a53b44bb41ca3b3b8ea4b55073200075e73d5
Christopher Rojas <christopherrojas35@yahoo.com>
2020-10-04 13:19:24 -0600

    added an id to the db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,21 @@
 //Data to load
 var notesDB = require('../db/db.json')
+var fs = require('fs');
+//routing 
+function apiRoute (app){
+
+    //display to user the JSON of the Database
+    app.get('/api/notes', function(req, res){
+        res.json(notesDB);
+    });
+
+    app.post('/api/notes', function(req, res){
+        notesDB.push(req.body);
+    });
+
+    app.delete('/api/notes/:id', function(req, res){
+        
+    });
+};
+
+module.exports = apiRoute;
--- server.js
+++ server.js
@@ -9,10 +9,9 @@ var PORT = process.env.PORT || 3001
 //handle data parsing 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static)
 
 
-// require("./routes/apiRoutes")(app);
+require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function(){
