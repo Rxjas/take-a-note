b1254b403461b25b5220f2a08a5dc18ee41f0749
Christopher Rojas <christopherrojas35@yahoo.com>
2020-10-04 12:41:14 -0600

    added routes folder and created server

---
+++ server.js
@@ -0,0 +1,20 @@
+//dependencies
+var express = require("express");
+var path = require("path");
+
+//create the server and choose the port
+var app = express();
+var PORT = process.env.PORT || 3001
+
+//handle data parsing 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.static)
+
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function(){
+    console.log("Server is listening on port: ", PORT)
+})
