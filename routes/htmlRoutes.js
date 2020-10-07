a2045759089903a68c06ab54d6ed51e3ca3724ff
Christopher Rojas <christopherrojas35@yahoo.com>
2020-10-07 16:48:46 -0600

    changed package.json and html routes for heroku

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,13 +5,17 @@ var path = require('path')
 
 function router (app){
 
+    app.get("/", function(req, res) {
+        res.json(path.join(__dirname, "../public/index.html"));
+      });
+
     app.get('/notes', function(req, res){
         res.sendFile(path.join(__dirname, "../public/notes.html"));
-    })
+    });
 
     app.get('*', function(req, res){
         res.sendFile(path.join(__dirname, "../public/index.html"));
-    })
+    });
 };
 
 module.exports = router;
